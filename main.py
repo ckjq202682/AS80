19969a7c55c7a5e636b4d4d2f380ff900f728888
ckjq202682 <63698151+ckjq202682@users.noreply.github.com>
2020-10-12 17:44:23 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# AS80
+
+r = int(input("Circle Radius"))
+
+def area(r):
+    return r**2 * 3.141592
+
+def circumference(r):
+    return r * 2 * 3.141592
+
+print("Area:", area(r))
+print("circumference:", circumference(r))
